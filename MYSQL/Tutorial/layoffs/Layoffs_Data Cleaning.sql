5f3e34826682b46517a4934b0b1cba7b3a0a084f
Subham Kumar <158332737+960Subhamsingh@users.noreply.github.com>
2024-12-18 00:41:36 +0530

    layoffs_data cleaning

--- MYSQL/Tutorial/layoffs/Layoffs_Data Cleaning.sql
+++ MYSQL/Tutorial/layoffs/Layoffs_Data Cleaning.sql
@@ -1,8 +1,10 @@
 /* first thing we want to do is create a layoffs_staging  table. 
 This is the one we will work in and clean the data. We want a table with the raw data in case something happens
 */
+
 CREATE TABLE layoffs.layoffs_staging 
 LIKE layoffs.layoffs;
+
 -- Insert the 'LayOff_Staging' table using the 'Playoffs' table
 INSERT INTO layoffs.layoffs_staging  
 SELECT * FROM layoffs.layoffs;
@@ -11,7 +13,7 @@ SELECT *
 FROM (
 	SELECT company, industry, total_laid_off,`date`,
 		ROW_NUMBER() OVER (
-			PARTITION BY company, industry, total_laid_off,`date`
+			PARTITION BY company,`date`
 			) AS row_num
 	FROM 
 		layoffs.layoffs_staging
@@ -36,7 +38,7 @@ SELECT * FROM (
 			PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions
 			) AS row_num
 	FROM 
-		layoffs.layoffs_staging
+		layoffs.layoffs_staging2
 ) duplicates
 WHERE 
 	row_num > 1;
@@ -75,6 +77,7 @@ WHERE (company, location, industry, total_laid_off, percentage_laid_off, `date`,
 -- so let's do it!!
 
 ALTER TABLE layoffs.layoffs_staging ADD row_num INT;
+ALTER TABLE layoffs.layoffs_staging drop row_num;
 
 SELECT *
 FROM layoffs.layoffs_staging;
@@ -124,34 +127,43 @@ DELETE FROM layoffs.layoffs_staging2 WHERE row_num >= 2;
 
 select * from layoffs.layoffs_staging2;
 
-
-
-
-
 -- 2. Standardize Data
 
-SELECT * 
-FROM world_layoffs.layoffs_staging2;
+SELECT 
+    *
+FROM
+    layoffs.layoffs_staging2;
 
 -- if we look at industry it looks like we have some null and empty rows, let's take a look at these
-SELECT DISTINCT industry
-FROM world_layoffs.layoffs_staging2
+SELECT DISTINCT
+    industry
+FROM
+    layoffs.layoffs_staging2
 ORDER BY industry;
 
-SELECT *
-FROM world_layoffs.layoffs_staging2
-WHERE industry IS NULL 
-OR industry = ''
+SELECT 
+    *
+FROM
+    layoffs.layoffs_staging2
+WHERE
+    industry IS NULL OR industry = ''
 ORDER BY industry;
 
 -- let's take a look at these
-SELECT *
-FROM world_layoffs.layoffs_staging2
-WHERE company LIKE 'Bally%';
+SELECT 
+    *
+FROM
+    layoffs.layoffs_staging2
+WHERE
+    company LIKE 'Bally%';
+    
 -- nothing wrong here
-SELECT *
-FROM world_layoffs.layoffs_staging2
-WHERE company LIKE 'airbnb%';
+SELECT 
+    *
+FROM
+    layoffs.layoffs_staging2
+WHERE
+    company LIKE 'airbnb%';
 
 -- it looks like airbnb is a travel, but this one just isn't populated.
 -- I'm sure it's the same for the others. What we can do is
@@ -159,89 +171,104 @@ WHERE company LIKE 'airbnb%';
 -- makes it easy so if there were thousands we wouldn't have to manually check them all
 
 -- we should set the blanks to nulls since those are typically easier to work with
-UPDATE world_layoffs.layoffs_staging2
-SET industry = NULL
-WHERE industry = '';
+UPDATE layoffs.layoffs_staging2 
+SET 
+    industry = NULL
+WHERE
+    industry = '';
 
 -- now if we check those are all null
 
-SELECT *
-FROM world_layoffs.layoffs_staging2
-WHERE industry IS NULL 
-OR industry = ''
+SELECT 
+    *
+FROM
+    layoffs.layoffs_staging2
+WHERE
+    industry IS NULL OR industry = ''
 ORDER BY industry;
 
 -- now we need to populate those nulls if possible
 
-UPDATE layoffs_staging2 t1
-JOIN layoffs_staging2 t2
-ON t1.company = t2.company
-SET t1.industry = t2.industry
-WHERE t1.industry IS NULL
-AND t2.industry IS NOT NULL;
+UPDATE layoffs.layoffs_staging2 t1
+        JOIN
+    layoffs.layoffs_staging2 t2 ON t1.company = t2.company 
+SET 
+    t1.industry = t2.industry
+WHERE
+    t1.industry IS NULL
+        AND t2.industry IS NOT NULL;
 
 -- and if we check it looks like Bally's was the only one without a populated row to populate this null values
-SELECT *
-FROM world_layoffs.layoffs_staging2
-WHERE industry IS NULL 
-OR industry = ''
+SELECT 
+    *
+FROM
+    layoffs.layoffs_staging2
+WHERE
+    industry IS NULL OR industry = ''
 ORDER BY industry;
 
 -- ---------------------------------------------------
 
 -- I also noticed the Crypto has multiple different variations. We need to standardize that - let's say all to Crypto
-SELECT DISTINCT industry
-FROM world_layoffs.layoffs_staging2
+SELECT DISTINCT
+    industry
+FROM
+    layoffs.layoffs_staging2
 ORDER BY industry;
 
-UPDATE layoffs_staging2
-SET industry = 'Crypto'
-WHERE industry IN ('Crypto Currency', 'CryptoCurrency');
+UPDATE layoffs.layoffs_staging2 
+SET 
+    industry = 'Crypto'
+WHERE
+    industry IN ('Crypto Currency' , 'CryptoCurrency');
 
 -- now that's taken care of:
-SELECT DISTINCT industry
-FROM world_layoffs.layoffs_staging2
+SELECT DISTINCT
+    industry
+FROM
+    layoffs.layoffs_staging2
 ORDER BY industry;
 
 -- --------------------------------------------------
 -- we also need to look at 
 
-SELECT *
-FROM world_layoffs.layoffs_staging2;
+SELECT 
+    *
+FROM
+    layoffs.layoffs_staging2;
 
 -- everything looks good except apparently we have some "United States" and some "United States." with a period at the end. Let's standardize this.
-SELECT DISTINCT country
-FROM world_layoffs.layoffs_staging2
+SELECT DISTINCT
+    country
+FROM
+    layoffs.layoffs_staging2
 ORDER BY country;
 
-UPDATE layoffs_staging2
+UPDATE layoffs.layoffs_staging2
 SET country = TRIM(TRAILING '.' FROM country);
 
 -- now if we run this again it is fixed
-SELECT DISTINCT country
-FROM world_layoffs.layoffs_staging2
+SELECT DISTINCT
+    country
+FROM
+    layoffs.layoffs_staging2
 ORDER BY country;
 
 
 -- Let's also fix the date columns:
-SELECT *
-FROM world_layoffs.layoffs_staging2;
+SELECT 
+    *
+FROM
+    layoffs.layoffs_staging2;
 
 -- we can use str to date to update this field
-UPDATE layoffs_staging2
-SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+UPDATE layoffs.layoffs_staging2 
+SET 
+    `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
 
 -- now we can convert the data type properly
-ALTER TABLE layoffs_staging2
+ALTER TABLE layoffs.layoffs_staging2
 MODIFY COLUMN `date` DATE;
-
-
-SELECT *
-FROM world_layoffs.layoffs_staging2;
-
-
-
-
 
 -- 3. Look at Null Values
 
@@ -250,39 +277,30 @@ FROM world_layoffs.layoffs_staging2;
 
 -- so there isn't anything I want to change with the null values
 
+-- 4. Delete any columns and rows we don't need
 
-
-
--- 4. remove any columns and rows we need to
-
-SELECT *
-FROM world_layoffs.layoffs_staging2
-WHERE total_laid_off IS NULL;
-
-
-SELECT *
-FROM world_layoffs.layoffs_staging2
-WHERE total_laid_off IS NULL
-AND percentage_laid_off IS NULL;
-
--- Delete Useless data we can't really use
 SELECT 
     *
 FROM
-    layoffs.layoffs_staging2  WHERE
-    total_laid_off IS NULL   AND percentage_laid_off IS NULL;
+    layoffs.layoffs_staging2
+WHERE
+    total_laid_off IS NULL
+        AND percentage_laid_off IS NULL;
+        
+-- Delete Useless data we can't really use  
+-- Remove useless data. We aren't actually using the total_laid_off and percantage_laid_of columns.
 
 DELETE FROM layoffs.layoffs_staging2 
 WHERE
     total_laid_off IS NULL
     AND percentage_laid_off IS NULL;
 
+-- Drop row_num;
 ALTER TABLE layoffs_staging2
 DROP COLUMN row_num;
 
 SELECT * 
 FROM layoffs.layoffs_staging2;
-
 
 WITH DELETE_CTE AS (
     SELECT *,
